2698541dd20450a02b41855a1a72447aa6d9746f
Jrobbinsss18 <jrobbinsss18@gmail.com>
2023-11-13 16:40:04 -0800

    First commit

---
+++ Data_condensation.py
@@ -0,0 +1,19 @@
+import re
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import string
+import nltk
+import warnings 
+def maximum(a, b):
+     
+    if a >= b:
+        return a
+    else:
+        return b
+     
+# Driver code
+a = 2
+b = 4
+print(maximum(a, b)) 
